efaa7fc26a981a6e20e9b564c586e0c84bb31acd
mayankjindal-ktd <mayankjindal.ktd@gmail.com>
2017-02-20 19:40:57 +0530

    Add files via upload

---
+++ 1_5.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ float fahr,celsius;
+ int lower,upper,step;
+ clrscr();
+ lower=0;         /* lower limit of temperature scale*/
+ upper=300;       /* upper limit */
+ step=20;         /* step size */
+ fahr=upper;
+ printf("fehrenheit\tcelsius\n");
+ while(fahr>=lower)
+ {
+  celsius=5*(fahr-32)/9.0;
+  printf("%f\t%f\n",fahr,celsius);
+  fahr=fahr-step;
+ }
+ getch();
+}
