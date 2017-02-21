eb4692bfcd6d9f7b0e9724b25074697556d86aa3
mayankjindal-ktd <mayankjindal.ktd@gmail.com>
2017-02-21 23:46:53 +0530

    Add files via upload

---
+++ 1_13.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int c,word;
+ clrscr();
+ while((c=getchar())!=EOF)
+ {
+  if(c!=' ' && c!='\t')
+  {
+  printf("*");
+  word=0;
+  }
+  else if((c==' ' || c=='\t')&& word==0)
+  {
+   printf("\n");
+   ++word;
+  }
+ }
+ getch();
+}
